8a60253b4370dffa997b3c08ee39981b8c76d38e
Mark Manley <mark.manley3@icloud.com>
2024-11-26 21:51:13 -0500

    Rename: TryError.ErrCodeOptions -> .CodeOptions

--- src/errors/try-error.test.ts
+++ src/errors/try-error.test.ts
@@ -4,31 +4,31 @@ import { TryError } from "./try-error.js";
 suite("TryError", () => {
   test("1) with code: Nullish Value Caught -> creates an instance with the correct error code & cause & message", () => {
     const error = new TryError(
-      TryError.ErrCodeOptions.NULLISH_VALUE_CAUGHT,
+      TryError.CodeOptions.NULLISH_VALUE_CAUGHT,
       undefined,
     );
 
-    expect(error.code).toBe(TryError.ErrCodeOptions.NULLISH_VALUE_CAUGHT);
+    expect(error.code).toBe(TryError.CodeOptions.NULLISH_VALUE_CAUGHT);
     expect(error.cause).toBeUndefined();
     expect(error.message).toBe("A nullish value has been caught");
   });
   test("2) with code: Not A Function -> creates an instance with the correct error code & cause & message", () => {
     const error = new TryError(
-      TryError.ErrCodeOptions.NOT_A_FUNCTION,
+      TryError.CodeOptions.NOT_A_FUNCTION,
       "not a function",
     );
 
-    expect(error.code).toBe(TryError.ErrCodeOptions.NOT_A_FUNCTION);
+    expect(error.code).toBe(TryError.CodeOptions.NOT_A_FUNCTION);
     expect(error.cause).toBe("not a function");
     expect(error.message).toBe("The 'fn' argument is not a function");
   });
   test("3) with code: Not An Array -> creates an instance with the correct error code & cause & message", () => {
     const error = new TryError(
-      TryError.ErrCodeOptions.NOT_AN_ARRAY,
+      TryError.CodeOptions.NOT_AN_ARRAY,
       "not an array",
     );
 
-    expect(error.code).toBe(TryError.ErrCodeOptions.NOT_AN_ARRAY);
+    expect(error.code).toBe(TryError.CodeOptions.NOT_AN_ARRAY);
     expect(error.cause).toBe("not an array");
     expect(error.message).toBe("The 'listOfFns' argument is not an array");
   });
--- src/errors/try-error.ts
+++ src/errors/try-error.ts
@@ -5,21 +5,21 @@
 // } as const;
 
 export type TryErrorCode =
-  (typeof TryError.ErrCodeOptions)[keyof typeof TryError.ErrCodeOptions];
+  (typeof TryError.CodeOptions)[keyof typeof TryError.CodeOptions];
 
 function tryErrorMessageFromCode(code: TryErrorCode): string {
   switch (code) {
-    case TryError.ErrCodeOptions.NULLISH_VALUE_CAUGHT:
+    case TryError.CodeOptions.NULLISH_VALUE_CAUGHT:
       return "A nullish value has been caught";
-    case TryError.ErrCodeOptions.NOT_A_FUNCTION:
+    case TryError.CodeOptions.NOT_A_FUNCTION:
       return "The 'fn' argument is not a function";
-    case TryError.ErrCodeOptions.NOT_AN_ARRAY:
+    case TryError.CodeOptions.NOT_AN_ARRAY:
       return "The 'listOfFns' argument is not an array";
   }
 }
 
 export class TryError extends Error {
-  static ErrCodeOptions = {
+  static CodeOptions = {
     NULLISH_VALUE_CAUGHT: "ERR_NULLISH_VALUE_CAUGHT",
     NOT_A_FUNCTION: "ERR_NOT_A_FUNCTION",
     NOT_AN_ARRAY: "ERR_NOT_AN_ARRAY",
@@ -27,19 +27,19 @@ export class TryError extends Error {
 
   static NullishValueCaught = class NullishValueCaught extends TryError {
     constructor(cause: null | undefined) {
-      super(TryError.ErrCodeOptions.NULLISH_VALUE_CAUGHT, cause);
+      super(TryError.CodeOptions.NULLISH_VALUE_CAUGHT, cause);
     }
   };
 
   static NotAFunction = class NotAFunction extends TryError {
     constructor(cause: null | undefined) {
-      super(TryError.ErrCodeOptions.NOT_A_FUNCTION, cause);
+      super(TryError.CodeOptions.NOT_A_FUNCTION, cause);
     }
   };
 
   static NotAnArray = class NotAnArray extends TryError {
     constructor(cause: null | undefined) {
-      super(TryError.ErrCodeOptions.NOT_AN_ARRAY, cause);
+      super(TryError.CodeOptions.NOT_AN_ARRAY, cause);
     }
   };
 
