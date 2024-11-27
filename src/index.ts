6fad361036504ebe09b4b4902db808c16ed9f9d7
Mark Manley <mark.manley3@icloud.com>
2024-11-27 00:13:00 -0500

    Create initial 'index.ts' main file

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+export { TryError } from "./errors/try-error.js";
+export { GenericError } from "./errors/_000-generic-error.js";
+export { UnresolvableError } from "./errors/_600-unresolvable-error.js";
+
+export { ensureError } from "./lib/ensure-error.js";
+export { panic } from "./lib/panic.js";
+
+export { tryTo } from "./lib/try-to.js";
+// tryToCurried
+export { syncTry } from "./lib/sync-try.js";
+export { asyncTry } from "./lib/async-try.js";
+// tryMulti
+// tryMultiCurried
+// tryPipe
+// tryPipeCurried
+// tryEach
+// tryEachCurried
